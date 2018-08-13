28ee5dbc2d0c102cf07f6e44814e09d9fe333424
Alex Marichal <marichalalex@yahoo.fr>
2018-08-13 14:51:30 +0200

    Done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,32 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = [*('A'..'Z')].sample(8)
+  end
+
+  def score
+    @answer = params[:answer].upcase
+    @letters = params[:word]
+    array = @answer.chars.select { |letter| @letters.chars.include?(letter) }
+    if array.count != @answer.chars.count
+      @result = "Not in the grid :/"
+      @score = 0
+    elsif get_json(@answer)["found"] == false
+      @result = "Not english my friend"
+      @score = 0
+    else
+      @result = "Well done buddy!"
+      @score = @answer.chars.count
+    end
+  end
+
+  def get_json(answer)
+    url = "https://wagon-dictionary.herokuapp.com/#{answer}"
+    serialized_grid = open(url).read
+    result = JSON.parse(serialized_grid)
+    return result
+  end
+
+end
